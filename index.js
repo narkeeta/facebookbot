0968ab4368c4a8f03b2ce5737c878cec3e5baea3
Collin McCabe <cmccabe4363@gmail.com>
2017-02-17 22:52:02 -0700

    Starter Upload

---
+++ index.js
@@ -0,0 +1,249 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === '2347234dds772347234') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function () {
+	console.log('running on port', app.get('port'))
+})
+
+//
+//
+// A majority of the bot code here
+//
+//
+
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i];
+		let sender = event.sender.id;
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			if (text === 'Generic') {
+				sendGenericMessage(sender)
+				continue
+			}
+			if (text === 'Lincoln') {
+				let payload = event.message.quick_reply.payload;
+				if (payload === 'USER_CHOSE_LINCOLN') {
+					sendTextMessage(sender, "I do love a good party in Lincoln 💃💃💃", token)
+					askLincolnEvents(sender)
+				}
+				continue
+			}
+			sendStarterButtons(sender)
+		}
+		if (event.postback) {
+			let text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+			continue
+		}
+	}
+	res.sendStatus(200)
+})
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+//
+//
+// THESE ARE THE MESSAGE FUNCTIONS
+//
+//
+
+function sendTextMessage(sender, text) {
+	let messageData = {
+		text: text
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: token
+		},
+		method: 'POST',
+		json: {
+			recipient: {
+				id: sender
+			},
+			message: messageData,
+		}
+	}, function (error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+function sendGenericMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: token
+		},
+		method: 'POST',
+		json: {
+			recipient: {
+				id: sender
+			},
+			message: messageData,
+		}
+	}, function (error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}   
+
+function askLincolnEvents(sender) {
+	let messageData = {
+		"text":"What club events in Lincoln would you like me to remind you for? 🙌🙌",
+		"quick_replies":[
+			{
+				"content_type":"text",
+				"title":"Superbull",
+				"payload":"USER_EVENT_LINCOLN_SUPERBULL"
+			},
+			{
+				"content_type":"text",
+				"title":"Union",
+				"payload":"USER_EVENT_LINCOLN_UNION"
+			},
+			{
+				"content_type":"text",
+				"title":"Entourage",
+				"payload":"USER_EVENT_LINCOLN_ENTROURAGE"
+			},
+			{
+				"content_type":"text",
+				"title":"Quack",
+				"payload":"USER_EVENT_LINCOLN_QUACK"
+			},
+			{
+				"content_type":"text",
+				"title":"Lovedough",
+				"payload":"USER_EVENT_LINCOLN_LOVEDOUGH"
+			},
+			{
+				"content_type":"text",
+				"title":"All",
+				"payload":"USER_EVENT_LINCOLN_ALL"
+			}
+		]
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: token
+		},
+		method: 'POST',
+		json: {
+			recipient: {
+				id: sender
+			},
+			message: messageData,
+		}
+	}, function (error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+function sendStarterButtons(sender) {
+	let messageData = {
+		"text":"Hi there🙋 I'm here to make sure you never miss out on an absolutely banging time! 😎 I'll do this by reminding you to buy tickets for your fave events! Let me know what city you're in and let's get onnit 🍻",
+		"quick_replies":[
+			{
+				"content_type":"text",
+				"title":"Lincoln",
+				"payload":"USER_CHOSE_LINCOLN"
+			},
+			{
+				"content_type":"text",
+				"title":"Sheffield",
+				"payload":"USER_CHOSE_SHEFFIELD"
+			}
+		]
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: token
+		},
+		method: 'POST',
+		json: {
+			recipient: {
+				id: sender
+			},
+			message: messageData,
+		}
+	}, function (error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
